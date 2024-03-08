0e9a6ad559a3968f691db9ab19dbc7ba0bafa7fb
sukmuzi <sukmuzi@naver.com>
2024-03-09 03:57:07 +0900

    [AF-135] feat: 마이페이지 회원정보 컴포넌트 수정

--- app/_components/mypage/Member.tsx
+++ app/_components/mypage/Member.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import Input from '../Input';
 
 export default function Member() {
-  return <div>Member</div>;
+  const handleOnChange = () => {
+    console.log('onChange !!');
+  };
+
+  return (
+    <div className="m-auto w-96 mt-[60px]">
+      {/* <span className="text-black text-base font-normal font-['ABeeZee']">이메일</span> */}
+      <Input
+        label="이메일"
+        name="email"
+        className="w-full h-[50px] bg-white rounded-md border border-solid border-[rgba(56.18, 70.02, 95.62, 0.15)]"
+        onChange={handleOnChange}
+        placeholder=""
+        value=""
+      />
+      <Input
+        label="이메일"
+        name="email"
+        className="w-full h-[50px] bg-white rounded-md border border-solid border-[rgba(56.18, 70.02, 95.62, 0.15)]"
+        onChange={handleOnChange}
+        placeholder=""
+        value=""
+      />
+    </div>
+  );
 }
