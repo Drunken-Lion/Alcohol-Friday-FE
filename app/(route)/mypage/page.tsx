1f753f4b2fbeb5cad5d5ff6b1d11dd4f665346a8
sukmuzi <sukmuzi@naver.com>
2024-03-09 03:56:29 +0900

    [AF-31] 마이페이지 사용자 닉네임 추가

--- app/(route)/mypage/page.tsx
+++ app/(route)/mypage/page.tsx
@@ -6,18 +6,20 @@ import Navigation from 'app/_components/mypage/Navigation';
 import Order from 'app/_components/mypage/Order';
 import OrderStatus from 'app/_components/mypage/OrderStatus';
 import Question from 'app/_components/mypage/Question';
+import { useSession } from 'next-auth/react';
 import React, { useState } from 'react';
 
 export default function mypage() {
   const [selectedTab, setSelectedTab] = useState<string>('order');
+  const { data: session } = useSession();
 
   const handleTabClick = (tabName: string) => {
     setSelectedTab(tabName);
   };
 
   return (
-    <div className="h-screen w-screen">
-      <Navigation nickname="nickname" onClick={handleTabClick} />
+    <div className="w-screen">
+      <Navigation nickname={session?.user.nickname} onClick={handleTabClick} />
       {selectedTab === 'order' && <Order />}
       {selectedTab === 'review' && <OrderStatus count={20} />}
       {selectedTab === 'member' && <Member />}
