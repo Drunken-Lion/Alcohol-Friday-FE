c880bc7da9856eff18940a1bd0245ca1a0e42252
aronglee <arong16@naver.com>
2024-03-14 19:07:37 +0900

    [AF-218] feat : add store detail page link tag

--- app/_components/store/ProductList.tsx
+++ app/_components/store/ProductList.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ProductItem from './ProductItem';
+import Link from 'next/link';
 
 export default function ProductList() {
+  const test = 1;
   return (
     <div className="grid grid-cols-4 gap-6 mx-36">
-      <ProductItem
-        image="../images/alcohol.png"
-        name="북한강"
-        category="증류수"
-        price="10,000"
-        discountRate={20}
-        discountPrice="8,000"
-        reviewPoint={4.5}
-        reviewCount={125}
-      />
+      <Link href={`/store/${test}`}>
+        <ProductItem
+          image="../images/alcohol.png"
+          name="북한강"
+          category="증류수"
+          price="10,000"
+          discountRate={20}
+          discountPrice="8,000"
+          reviewPoint={4.5}
+          reviewCount={125}
+        />
+      </Link>
       <ProductItem
         image="../images/alcohol.png"
         name="북한강"
