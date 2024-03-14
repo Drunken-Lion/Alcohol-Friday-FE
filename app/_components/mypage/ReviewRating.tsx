6a00297be6e1a747f38da126338f08194ffaf5f1
aronglee <arong16@naver.com>
2024-03-14 19:10:43 +0900

    [AF-131] feat : add isShow checking

--- app/_components/mypage/ReviewRating.tsx
+++ app/_components/mypage/ReviewRating.tsx
@@ -3,28 +3,38 @@ import { FaStar } from 'react-icons/fa';
 
 interface ReviewRatingProps {
   clicked: boolean[];
-  onStarClick: (index: number) => void;
+  onStarClick?: ((index: number) => void) | null;
+  isShow: boolean;
 }
 
-export default function ReviewRating({ clicked, onStarClick }: ReviewRatingProps) {
+export default function ReviewRating({ clicked, onStarClick, isShow }: ReviewRatingProps) {
   const starArray = [0, 1, 2, 3, 4];
 
   return (
     <div className="flex">
-      <div className="flex flex-row">
-        {starArray.map((el) => {
-          const starStyles = clicked[el] ? 'text-blue-950' : 'text-slate-300';
-          return (
-            <FaStar
-              fontSize={30}
-              key={el}
-              id={`${el}`}
-              onClick={() => onStarClick(el)}
-              className={`${starStyles} cursor-grabbing`}
-            />
-          );
-        })}
-      </div>
+      {isShow ? (
+        <div className="flex flex-row">
+          {starArray.map((el) => {
+            const starStyles = clicked[el] ? 'text-blue-950' : 'text-slate-300';
+            return <FaStar fontSize={20} key={el} id={`${el}`} className={`${starStyles}`} />;
+          })}
+        </div>
+      ) : (
+        <div className="flex flex-row">
+          {starArray.map((el) => {
+            const starStyles = clicked[el] ? 'text-blue-950' : 'text-slate-300';
+            return (
+              <FaStar
+                fontSize={30}
+                key={el}
+                id={`${el}`}
+                onClick={() => onStarClick && onStarClick(el)}
+                className={`${starStyles} cursor-grabbing`}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
