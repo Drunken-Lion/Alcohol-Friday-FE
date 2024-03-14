1a035d4054f93580b7cbbd21bb752ec0b99c184d
aronglee <arong16@naver.com>
2024-03-14 19:08:55 +0900

    [AF-218] feat : add detail page check

--- app/_components/store/ProductItem.tsx
+++ app/_components/store/ProductItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Star from '/public/images/star.svg';
+import ReviewRating from '../mypage/ReviewRating';
 
 interface ProductItemProps {
   image: string;
@@ -10,6 +11,7 @@ interface ProductItemProps {
   discountPrice: string;
   reviewPoint: number;
   reviewCount: number;
+  detailCheck?: boolean;
 }
 
 export default function ProductItem({
@@ -21,28 +23,51 @@ export default function ProductItem({
   discountPrice,
   reviewPoint,
   reviewCount,
+  detailCheck,
 }: ProductItemProps) {
   return (
-    <div className="flex flex-col gap-5">
+    <div className={`flex ${detailCheck ? 'flex-row' : 'flex-col'} gap-5`}>
       <img
         src={image}
         className="bg-white rounded-lg border border-slate-700 border-opacity-20 py-10 px-28"
       />
-      <div>
-        <p className="text-zinc-800 text-xl font-normal">{name}</p>
-        <p className="text-stone-500 text-base font-normal py-2">{category}</p>
-        <p className="text-gray-300 text-base font-normal line-through">{price}</p>
-        <div className="flex gap-2">
-          <span className="text-red-500 text-2xl font-bold">{discountRate}%</span>
-          <span className="text-zinc-800 text-2xl font-bold">{discountPrice}</span>
+      {detailCheck ? (
+        <div>
+          <p className="text-stone-500 text-2xl font-bold pb-5">{name}</p>
+          <p className="text-neutral-400 text-xl font-bold pb-2">{category}</p>
+          <div className="flex pb-16 gap-1">
+            <ReviewRating
+              clicked={[true, true, true, false, false]}
+              onStarClick={undefined}
+              isShow={true}
+            />
+            <span className="text-neutral-400 text-xl font-normal">
+              {reviewPoint} ({reviewCount})
+            </span>
+          </div>
+          <p className="text-gray-300 text-xl font-normal line-through">{price}</p>
+          <div className="flex gap-2">
+            <span className="text-red-500 text-3xl font-bold">{discountRate}%</span>
+            <span className="text-zinc-800 text-3xl font-bold">{discountPrice}</span>
+          </div>
         </div>
-        <div className="flex pt-2 gap-1">
-          <Star />
-          <span className="text-neutral-400 text-base font-normal">
-            {reviewPoint} ({reviewCount})
-          </span>
+      ) : (
+        <div>
+          <p className="text-zinc-800 text-xl font-normal">{name}</p>
+          <p className="text-stone-500 text-base font-normal py-2">{category}</p>
+          <p className="text-gray-300 text-base font-normal line-through">{price}</p>
+          <div className="flex gap-2">
+            <span className="text-red-500 text-2xl font-bold">{discountRate}%</span>
+            <span className="text-zinc-800 text-2xl font-bold">{discountPrice}</span>
+          </div>
+          <div className="flex pt-2 gap-1">
+            <Star />
+            <span className="text-neutral-400 text-base font-normal">
+              {reviewPoint} ({reviewCount})
+            </span>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
