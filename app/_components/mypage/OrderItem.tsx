5064597f45bb9e16ea95061c87656ed77038d22a
aronglee <arong16@naver.com>
2024-03-16 21:20:47 +0900

    [AF-148] fix : add cartCheck UI

--- app/_components/mypage/OrderItem.tsx
+++ app/_components/mypage/OrderItem.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '../Button';
 import Portal from '../Portal';
+import { FiMinus } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
 
 interface OrderItemProps {
   storeName: string;
@@ -9,8 +11,9 @@ interface OrderItemProps {
   price: string;
   quantity: number;
   image: string;
-  isValue: Boolean;
+  isValue?: Boolean;
   onClick?: () => void;
+  cartCheck?: Boolean;
 }
 
 export default function OrderItem({
@@ -22,13 +25,15 @@ export default function OrderItem({
   image,
   isValue,
   onClick,
+  cartCheck,
 }: OrderItemProps) {
+  console.log(cartCheck);
   return (
     <div className="flex flex-row gap-5">
       <div className="flex justify-center items-center w-28 h-40 bg-white rounded-lg border border-slate-700 border-opacity-20">
         <img src={image} />
       </div>
-      <div className="flex flex-col">
+      <div className={`flex flex-col ${cartCheck && 'w-full'}`}>
         <div className="flex text-zinc-800 text-base font-bold">
           {isValue && <span>[{storeName}]</span>}
           <span>{title}</span>
@@ -51,6 +56,23 @@ export default function OrderItem({
               portalName="리뷰쓰기"
               className="flex justify-center py-2 text-blue-900 text-sm font-normal w-48 bg-white rounded-lg border border-blue-900 cursor-grabbing"
             />
+          </div>
+        )}
+        {cartCheck && (
+          <div>
+            <div className="text-stone-500 text-sm font-normal pb-10">
+              <span>{price}</span>
+              <span className="px-1">/</span>
+              <span>수량 {quantity}개</span>
+            </div>
+            <div className="flex text-stone-500 justify-between">
+              <div className="flex">
+                <FiMinus className="cursor-grabbing" />
+                <span className="text-center text-base font-bold px-6">{quantity}</span>
+                <FiPlus className="cursor-grabbing" />
+              </div>
+              <div className="text-zinc-800 text-xl font-bold">3,000,000원</div>
+            </div>
           </div>
         )}
       </div>
